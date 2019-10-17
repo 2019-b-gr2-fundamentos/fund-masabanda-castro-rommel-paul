265e0bf7d1c4c08d3c51eb45956662b1ef999838
Rommel Masabanda <rommel.masabanda@epn.edu.ec>
2019-10-17 18:00:03 -0500

    guardar-cambios
    palabras reservadas

---
+++ lenguajes/01-variables.js
@@ -0,0 +1,30 @@
+/* 
+Nombre: Rommel Masabanda
+Fecha actualizacion: 17-10-2019
+Descripcion: Ejemplos de variables primitivas en JS
+*/
+//comentario
+/* <- abro bloque comentario
+cierro bloque comentario -> */
+
+// Primitivas (tipos de variables)
+/*
+int          -> numero ENTERO        EJ: 8 O0 O -6...
+char         -> caracter             EJ: a o A o b o c... 
+float        -> numeros decimal      EJ: 10.12 O -4.5
+double       -> n. decimales ++      EJ: 123132....... 
+string       -> cadena de caracteres Ej: "abc" o "cd"   
+null         -> vacio                EJ: null 
+boolean      -> vedardero/falso      Ej: trueo false
+*/
+// var edad = 20: (ya no se usa) //
+//let edad = 20; // mutable-> cambia
+const edad = 20 ; // inmutable-> no cambia
+//const IVA = 12;
+// IVA = 14: // NO REASIGNAR VALORES
+//Let sueldo = 0;
+
+// para phyton edad = 30 sin puntos y coma
+
+// var edad: Integer = 30; -> kotlin mutable
+// val edad: Integer = 30; -> Kotlin Inmutable
