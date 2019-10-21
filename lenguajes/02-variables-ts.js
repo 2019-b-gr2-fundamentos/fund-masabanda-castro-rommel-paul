f7bee72769b61f8f2497f362117966deca6b6096
Diego-Pullotasig <diego.pullotasig@epn.edu.ec>
2019-10-21 15:51:13 -0500

    Clase 08
    varaibales primitivas

---
+++ lenguajes/02-variables-ts.js
@@ -0,0 +1,10 @@
+// Variables primitivas
+var numero = 10;
+var decimal = 10.12;
+var respuestaTipoNumero = typeof numero;
+console.log(respuestaTipoNumero);
+console.log(typeof decimal);
+typeof decimal;
+//GESTOR DE PAQUETES DE JS
+//de node js
+// npm-> yarm
