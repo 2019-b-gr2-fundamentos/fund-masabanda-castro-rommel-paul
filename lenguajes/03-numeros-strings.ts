c19b480d9a3d0c04a57517179b0b89af9db37586
Diego-Pullotasig <diego.pullotasig@epn.edu.ec>
2019-10-23 17:56:25 -0500

    clase 9
    strings

---
+++ lenguajes/03-numeros-strings.ts
@@ -0,0 +1,40 @@
+let numeroUno = 1;  // notacion camelCase
+const numeroDos =2;
+let resultado = 0;
+// SUMA
+resultado = 1 + 2;
+// RESTAR
+resulatado = 1 - 2;
+// MULTIPLICAR
+resultado = 1 * 2;
+//EXPONENCIAL
+resultado = 1 ^ 2;
+// MOD
+resultado = 4 % 2; // 0 -> residuo
+resultado = 4 % 3; // 1 -> residuo
+resultado = 7 % 3; // 1 -> residuo 
+
+console.log("pera"+ 2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//NUMEROUNO   -> TODO MAYUSCULA (SEPARACION NO)
+//NUMERO_UNO  -> mayusculas guion bajo
+//NumeroUno   -> PrimeraLetraDeCadaPalabraConMayuscula
+//numerouno   -> 
+//numero_uno  -z
