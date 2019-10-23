0cc34210c999618a2923286214ce0adc25d66e46
Diego-Pullotasig <diego.pullotasig@epn.edu.ec>
2019-10-23 17:07:35 -0500

    deber
    hola

---
+++ DEBERES/DEBER 1/variables-primitivas.dart
@@ -0,0 +1,24 @@
+/*
+Nombre: Rommel Masabanda
+Fecha de actualizacion: 20/10/2019
+Descripcion: Variables primirivas en DART
+*/
+/* 
+En DART tenemos los sigueintes variables primitivas:
+NUMEROS-------------------------------------------
+num      -> entero o coma flotante                Ej: num pi = 3.141592
+int      -> enteros no mayores de 64bits          Ej: 88, 6457
+double   -> decimales, comoa flotate de 64 bits   Ej: 0, 0064871
+BOOLEANOS-----------------------------------------
+bool     -> verdadero o falso                     Ej: true o false
+CADENAS DE TEXTO----------------------------------
+String   -> cadena de caracteres                  Ej: noseradetomaruntragito
+LISTAS--------------------------------------------
+List     -> arreglo de un tipo de datos           Ej: [0,1,2,3,4,5]
+var      -> arreglo de varios tipos de datos      Ej: [27, "yael", false]
+MAPAS---------------------------------------------
+Map      -> estucturas de datos                   Ej: clave/valor
+*/
+ 
+
+
