526ba05d560d93df23dbde8de9e0510540df08ee
justinyamberla <55857299+justinyamberla@users.noreply.github.com>
2019-11-21 17:07:47 -0500

    funciones

---
+++ 03-funciones/01-introduccion.js
@@ -0,0 +1,15 @@
+// EN LA MAYOTIA DE LENGUANCES SE DEFINE PRIMERO LA FUNCION
+function log(cualquierCosa // PARAMETRO
+) {
+    console.log(cualquierCosa);
+}
+log("HoLA MunDO");
+// una funcion recive paramatreos(entrada) y devuelve una salida----un void es cuando no exisuten salidas
+function noHayElTexto() {
+    console.log(noHayElTexto);
+}
+noHayElTexto();
+function sumarDosNumeros(a, b) {
+    return a + b;
+}
+sumarDosNumeros(1, 2);
