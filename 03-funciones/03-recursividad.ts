a3905f913eeb7971494757a5653781c31896e848
Daniela Romero <daniela.romero@epn.edu.ec>
2019-11-27 18:02:30 -0500

    recursividad
    clase

---
+++ 03-funciones/03-recursividad.ts
@@ -0,0 +1,30 @@
+//recursividad para repetir el codigo como un while o un for
+function imrpimirMensajeNVeces(
+    mensaje: string,
+    numeroVeces: number,
+): void{
+    if(numeroVeces == 0){
+        console.log(`Se Termino`);
+    }else{
+        console.log(`hola`);
+        const nuevoNumeroVeces = numeroVeces -1;
+        imrpimirMensajeNVeces(mensaje, nuevoNumeroVeces);
+    }
+}
+function main(){
+    imrpimirMensajeNVeces (`hola`,8);
+}
+main()
+
+function detenerElArreglo(){
+
+}
+/* PSEUDOCODIGO
+1) Dado un conjunto de numeros(arreglo)
+2) Imprimir cada elemento del conjunto(arreglo) 
+*/
+
+function arregloDeNumeros(){
+    const elementos = [];
+    elementos
+}
