6a2719185ad50a94128e20a823b0a4477987b3e4
Rommel Masabanda <rommel.masabanda@epn.edu.ec>
2019-12-12 16:35:46 -0500

    Matriz

---
+++ 03-funciones/matrices.ts
@@ -0,0 +1,37 @@
+// dados dos matrices vamos a encontrar imprimer el orden de la matriz 
+const matrizA  = [[1,2],[2,4]];
+const matrizB = [[2,3],[8,0]];
+const matrizC = [[4,5,6],[7,6,3]];
+
+
+function verificarRango (matrizUno: number [],[] , matrizDos: number [],[]): boolean {
+    matrizUno.length //2
+    matrizDos.length //2
+    const filasA = matrizUno.length;
+
+    const columnasA: Number = matrizUno.length[0];
+
+    console.log(filasA);
+
+    
+    var filasB = matrizDos.length;
+    console.log(filasB);
+    if (filasA == filasB && ) {
+
+        
+    }
+    return true;
+
+}
+
+
+ 
+
+
+
+
+
+
+    
+
+
